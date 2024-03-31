49bf8cbd6f464b9d21b36bfd44661979da97c9b6
Charlon <charles.assus@gmail.com>
2024-03-31 12:29:28 +0700

    Preparing for v22

---
+++ src/Evergreen/V22/Types.elm
@@ -0,0 +1,193 @@
+module Evergreen.V22.Types exposing (..)
+
+import Browser
+import Browser.Navigation
+import Element
+import Evergreen.V22.Card
+import Lamdera
+import Random
+import Time
+import Url
+
+
+type alias FPlayer =
+    { name : String
+    , tableHand : List Evergreen.V22.Card.FCard
+    , clientId : Lamdera.ClientId
+    , sessionId : Lamdera.SessionId
+    , ready : Bool
+    , score : Maybe Int
+    }
+
+
+type alias TamalouOwner =
+    { sessionId : Lamdera.SessionId
+    , tableHand : List Evergreen.V22.Card.Card
+    }
+
+
+type alias FTableHand =
+    List Evergreen.V22.Card.FCard
+
+
+type alias FDrawPile =
+    List Evergreen.V22.Card.FCard
+
+
+type alias DiscardPile =
+    List Evergreen.V22.Card.Card
+
+
+type Counter
+    = Five
+    | Four
+    | Three
+    | Two
+    | One
+    | Zero
+
+
+type FPlayerToPlayStatus
+    = FWaitingPlayerAction (Maybe Evergreen.V22.Card.Power)
+    | FPlayerHasDraw Evergreen.V22.Card.FCard
+    | FPlayerHasDiscard Evergreen.V22.Card.Power
+    | FPlayerLookACard (Maybe Counter)
+
+
+type FGameInProgressStatus
+    = FStartTimerRunning Counter
+    | FPlayerToPlay Lamdera.SessionId FPlayerToPlayStatus
+    | FYourTurn FPlayerToPlayStatus
+    | FEndTimerRunning Counter
+
+
+type FGame
+    = FWaitingForPlayers (List FPlayer)
+    | FGameInProgress (Maybe TamalouOwner) FTableHand FDrawPile DiscardPile (List FPlayer) FGameInProgressStatus
+    | FGameEnded (List FPlayer)
+    | FGameAlreadyStartedWithoutYou
+
+
+type alias FrontendModel =
+    { key : Browser.Navigation.Key
+    , device : Element.Device
+    , fGame : FGame
+    , clientId : Maybe Lamdera.ClientId
+    , sessionId : Maybe Lamdera.SessionId
+    , urlPath : String
+    , errors : List String
+    , admin : Bool
+    , screenHeight : Int
+    , screenWidth : Int
+    , ready : Bool
+    , maybeName : Maybe String
+    }
+
+
+type alias BPlayer =
+    { name : String
+    , tableHand : List Evergreen.V22.Card.Card
+    , clientId : Lamdera.ClientId
+    , sessionId : Lamdera.SessionId
+    , ready : Bool
+    }
+
+
+type alias BDrawPile =
+    List Evergreen.V22.Card.Card
+
+
+type BPlayerToPlayStatus
+    = BWaitingPlayerAction (Maybe Evergreen.V22.Card.Power)
+    | BPlayerHasDraw Evergreen.V22.Card.Card
+    | BPlayerHasDiscard Evergreen.V22.Card.Power
+    | BPlayerLookACard (Maybe Counter)
+
+
+type BGameInProgressStatus
+    = BStartTimerRunning Counter
+    | BPlayerToPlay Lamdera.SessionId BPlayerToPlayStatus
+    | BEndTimerRunning Counter
+
+
+type BGameStatus
+    = BWaitingForPlayers (List BPlayer)
+    | BGameInProgress (Maybe Lamdera.SessionId) BDrawPile DiscardPile (List BPlayer) BGameInProgressStatus Bool Bool
+    | BGameEnded (List BPlayer)
+
+
+type alias BGame =
+    { urlPath : String
+    , status : BGameStatus
+    , seed : Random.Seed
+    }
+
+
+type alias BackendModel =
+    { games : List BGame
+    , errors : List String
+    , admins : List Lamdera.SessionId
+    }
+
+
+type FrontendMsg
+    = NoOpFrontendMsg
+    | UrlClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+    | GotWindowSize
+        { height : Int
+        , width : Int
+        }
+    | ChangeCurrentPlayerNameFrontend String
+    | StartGameFrontend
+    | ReStartGameFrontend
+    | DrawCardFromDeckFrontend
+    | TamalouFrontend
+    | DiscardCardFrontend
+    | DrawFromDiscardPileFrontend
+    | PowerIsUsedFrontend
+    | PowerPassFrontend
+    | ReplaceCardInFrontend Int
+    | DoubleCardFrontend Int
+    | LookAtCardFrontend Int
+
+
+type ActionFromGameToBackend
+    = ConnectToBackend
+    | ChangeCurrentPlayerNameToBackend String
+    | StartGameToBackend
+    | ReStartGameToBackend
+    | DrawCardFromDrawPileToBackend
+    | DiscardCardInHandToBackend
+    | DrawFromDiscardPileToBackend
+    | ReplaceCardInTableHandToBackend Int
+    | DoubleCardInTableHandToBackend Int
+    | LookAtCardInTableHandToBackend Int
+    | PowerIsUsedToBackend
+    | PowerIsNotUsedToBackend
+    | TamalouToBackend
+
+
+type ToBackend
+    = NoOpToBackend
+    | ActionFromGameToBackend String ActionFromGameToBackend
+    | ConnectToAdminToBackend
+
+
+type BackendMsgFromGame
+    = TimerTick Time.Posix
+    | CreateGame Time.Posix Lamdera.ClientId Lamdera.SessionId
+
+
+type BackendMsg
+    = NoOpBackendMsg
+    | FeedSessionIdAndClientId Lamdera.SessionId Lamdera.ClientId
+    | GotUserDisconnected Lamdera.SessionId Lamdera.ClientId
+    | BackendMsgFromGame String BackendMsgFromGame
+
+
+type ToFrontend
+    = NoOpToFrontend
+    | UpdateAdminToFrontend (List String)
+    | UpdateGameToFrontend FGame
+    | GotSessionIdAndClientIdToFrontend Lamdera.SessionId Lamdera.ClientId
