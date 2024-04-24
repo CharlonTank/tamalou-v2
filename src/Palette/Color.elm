87657f1c7d00ae5faae0b81039dee05acd430b3e
Charlon <charles.assus@gmail.com>
2024-04-24 11:45:19 +0700

    Fix middle text positioning + refacto a bit

---
+++ src/Palette/Color.elm
@@ -0,0 +1,23 @@
+module Palette.Color exposing (..)
+
+import Ui exposing (Color, rgb)
+
+
+yellow : Color
+yellow =
+    rgb 238 221 136
+
+
+blue : Color
+blue =
+    rgb 0 68 221
+
+
+green : Color
+green =
+    rgb 35 187 34
+
+
+red : Color
+red =
+    rgb 224 38 15
